0719e28cea245578f37cbe24665b053017df5d05
Antonio Zamora <zamora88875@gmail.com>
2019-05-21 13:29:26 -0700

    This is where it started.

---
+++ Testing.js
@@ -0,0 +1,9 @@
+function testings() {
+  let userName = document.getElementById("name").value
+  console.log(userName)
+  alert(userName + ", is it you?")
+}
+
+//Figure out how to create a function which takes in the
+//result from a form and console logs some response using
+//the input data.
