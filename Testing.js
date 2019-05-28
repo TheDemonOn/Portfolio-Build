8f6f321efdf644de97edcc2ca56873f01d434852
Antonio Zamora <zamora88875@gmail.com>
2019-05-28 16:32:05 -0700

    Trashed useless things.

--- Testing.js
+++
@@ -1,9 +0,0 @@
-function testings() {
-  let userName = document.getElementById("name").value
-  console.log(userName)
-  alert(userName + ", is it you?")
-}
-
-//Figure out how to create a function which takes in the
-//result from a form and console logs some response using
-//the input data.
